1d2b71a4be3af14f42dc501a3ba1b3dae6fa74ff
EthanNValencia <enephew2@mail.valenciacollege.edu>
2021-09-01 14:01:52 -0400

    Added documentation to the Fibonacci.java file.

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,10 +1,24 @@
+/***
+ * Class that contains a main method a recursive Fibonacci method. 
+ */
 public class Fibonacci {
 
+    /***
+     * This is the program main entry point.
+     * @author Ethan J. Nephew
+     * @param args command-line arguments.
+     */
     public static void main(String[] args){
         int i = 10;
         System.out.println("The " + i + "nth term of the Fibonacci sequence is " + recursiveFibonacci(i) + ".");
     }
 
+    /***
+     * A recursive Fibonacci method. The method calls itself until the check requirement is satisfied.
+     * @author Ethan J. Nephew
+     * @param n specifies the upper bound of the Fibonacci sequence.
+     * @return returns the Fib(x) in the form of an int.
+     */
     public static int recursiveFibonacci(int n) {
         if(n < 2){
             return n;
@@ -12,5 +26,4 @@ public class Fibonacci {
             return (recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2));
         }
     }
-
 }
