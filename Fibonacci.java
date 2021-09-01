3613a50908d4403621209abfcd02713fe7e2a3c8
EthanNValencia <enephew2@mail.valenciacollege.edu>
2021-09-01 13:40:53 -0400

    Adding the Fibonacci.java file to the repo.

---
+++ Fibonacci.java
@@ -0,0 +1,16 @@
+public class Fibonacci {
+
+    public static void main(String[] args){
+        int i = 10;
+        System.out.println("The " + i + "nth term of the Fibonacci sequence is " + recursiveFibonacci(i) + ".");
+    }
+
+    public static int recursiveFibonacci(int n) {
+        if(n < 2){
+            return n;
+        } else {
+            return (recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2));
+        }
+    }
+
+}
